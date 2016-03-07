f145b9800c5b636b17dc723794357e84aeb970dd
Kai Blaschke <kai.blaschke@kb-dev.net>
2016-03-07 17:14:02 +0100

    Updates site config files for Gluon 2016.x

--- site.mk
+++ site.mk
@@ -1,22 +1,39 @@
+##	GLUON_SITE_PACKAGES
+#		specify gluon/openwrt packages to include here
+#		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
 GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
 	gluon-alfred \
 	gluon-autoupdater \
-	gluon-config-mode \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-legacy \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
 	gluon-next-node \
-	gluon-mesh-batman-adv \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
+	gluon-setup-mode \
 	gluon-status-page \
-	iwinfo \
+	haveged \
 	iptables \
-	haveged
+	iwinfo
 
-DEFAULT_GLUON_RELEASE := 0.4+0-exp$(shell date '+%Y%m%d')
+# Allow overriding the release number from the command line
+DEFAULT_GLUON_RELEASE := 0.6+0-exp$(shell date '+%Y%m%d')
+
+# Default priority for updates.
+GLUON_PRIORITY ?= 0
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+# Languages to include
+GLUON_LANGS ?= en de
